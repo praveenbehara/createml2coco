64af9db9cc2af6a27340286e8669439ee8812041
Praveen Behara <praveen.behara@yahoo.com>
2022-05-25 01:51:01 +0530

    Change YOLO in variables to CreateML

--- createml2coco.py
+++ createml2coco.py
@@ -1,6 +1,7 @@
 # This is the 1st version of the code that I developed and assumed YOLO format to be JSON.
 # Only later I realized that this is code is written to convert CreateML format to COCO format.
 # This hasn't been well maintained, and needs a little more work
+# Even the config.json entries haven't been utilized
 
 from distutils.command.config import config
 import json
@@ -48,15 +49,15 @@ def group_annotations(coco_annos):
 pytesseract.tesseract_cmd = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
 custom_config = r"--oem 3 --psm 6"
 
-yolo_json_file = "vs_debug_error.json"
-with open(yolo_json_file, "r") as fp:
-    yolo_json = json.load(fp)
+createml_json_file = "vs_debug_error.json"
+with open(createml_json_file, "r") as fp:
+    createml_json = json.load(fp)
 
-img_file = Image.open(yolo_json[0]["image"])
+img_file = Image.open(createml_json[0]["image"])
 
 coco_annos = []
 
-for i, item in enumerate(yolo_json[0]["annotations"]):
+for i, item in enumerate(createml_json[0]["annotations"]):
     coco_anno = dict()
 
     coords = item["coordinates"]
@@ -87,7 +88,7 @@ coco_data = {}
 coco_data["form"] = grouped_coco_annos
 json_data = json.dumps(coco_data)
 
-folder, filename = os.path.split(yolo_json_file)
+folder, filename = os.path.split(createml_json_file)
 coco_filename = os.path.splitext(filename)[0] + "_coco.json"
 coco_file_full_path = os.path.join(folder, coco_filename)
 
